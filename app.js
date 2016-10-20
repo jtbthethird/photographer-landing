f5018019fbbab0b4c67bc1bf25f3b577766d52bb
J.T. Burgess <jt.burgess@mainstreethub.com>
2016-10-20 00:50:11 -0500

    Enable the form submission & receive it (but don't do anything) on the backend

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+app.post("/apply", function(req, res) {
+    console.log(req.body);
+    res.send();
+})
 
 app.listen(app.get('port'), function () {
     console.log('Photographer Landing Page listening on port ' + app.get('port') + '!');
--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,44 @@ $(function() {
     $("#example-link").on("click", function(e) {
         e.preventDefault();
         var photosSide = $("#photos-side");
+        var width = $("body").width();
+        photosSide.css("right", -width);
         photosSide.removeClass("hidden-xs");
+        photosSide.animate({"right": 0}, 300, "swing");
+        
     });
     
     $("#apply-link").on("click", function(e) {
         e.preventDefault();
         var photosSide = $("#photos-side");
-        photosSide.addClass("hidden-xs");
+        var width = $("body").width();
+        photosSide.animate({"right": -width}, 300, "swing", function() {
+            photosSide.addClass("hidden-xs");    
+        });
     });
-})
+    
+    var submitForm = function(e) {
+        console.log("Processing");
+        e.preventDefault();
+        console.log(e);
+        $.post("/apply", e);
+        return true;
+    };
+    
+    $("#photographer-form").parsley({ });
+    
+    $('#photographer-form').parsley().on('form:submit', function() {
+        var data = this.$element.serialize();
+        console.log(data);
+        var button = $("button#submit-form");
+        button.attr('disabled', 'disabled');
+        button.text("SUBMITTING");
+        
+        $.post("/apply", data, function(done) {
+            $("form#photographer-form input").attr('disabled', 'disabled');
+            button.text("THANKS!");
+        });
+        return false;
+    });
+
+});
